4671a8686e820124d57f0877b88f7986478ed62a
gustatarem <gus.tatarem@gmail.com>
2019-11-11 20:47:32 -0300

    Modified project layout

--- src/app/products.ts
+++ src/app/products.ts
@@ -1,18 +1,18 @@
 export const products = [
   {
-    name: 'Phone XL',
-    price: 799,
-    description: 'A large phone with one of the best screens'
+    name: 'iPhone 11 Pro Max',
+    price: 1199,
+    description: 'Pro cameras. Pro display. Pro performance.'
   },
   {
-    name: 'Phone Mini',
+    name: 'iPad Mini',
     price: 699,
-    description: 'A great phone with one of the best cameras'
+    description: 'Mini just got mightier.'
   },
   {
-    name: 'Phone Standard',
+    name: 'Apple Watch Series 3',
     price: 299,
-    description: ''
+    description: 'Stay active. Stay healthy. Stay connected.'
   }
 ];
 
